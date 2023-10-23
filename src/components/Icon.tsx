080881fc47dc0f65d5431454d0faa2457c2d4e8e
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-10-23 13:09:35 +0100

    fix iphone, align vertically on mobile

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -11,10 +11,10 @@ export const Icon = ({ className }: { className?: string }) => {
     >
         <motion.circle variants={getCirclesPathAnimationVariants()} className={styles.brown} cx="430.06" cy="430.06" r="429.06" />
         <motion.circle style={{strokeDashoffset: -2}} variants={getCirclesPathAnimationVariants()}  className={styles.black} cx="431.38" cy="429.36" r="388.73" />
-        <motion.text variants={getTextAnimation('below')} style={{fontSize: '4em'}} x="270" y="380" font-family="Garamond" fill="#914b45"> MARASCO </motion.text>
+        <motion.text variants={getTextAnimation('below')} style={{fontSize: '4em'}} x="270" y="380" fontFamily="Garamond" fill="#914b45"> MARASCO </motion.text>
         <motion.line variants={getCirclesPathAnimationVariants({delay: 2})} className={styles.brown} x1="105.19" y1="429.56" x2="430.1" y2="429.56" />
         <motion.line  style={{strokeDashoffset: -2}} variants={getCirclesPathAnimationVariants({delay: 2})} className={styles.brown} x1="430.1" y1="429.56" x2="755.7" y2="429.56" />
-        <motion.text variants={getTextAnimation('above')}  style={{fontSize: '4em'}} x="215" y="520" font-family="Garamond" fill="#914b45"> SANTANGELO </motion.text>
+        <motion.text variants={getTextAnimation('above')}  style={{fontSize: '4em'}} x="215" y="520" fontFamily="Garamond" fill="#914b45"> SANTANGELO </motion.text>
         <motion.line variants={getCirclesPathAnimationVariants({delay: 2})}  className={styles.brown} x1="430.1" y1="403.93" x2="430.1" y2="453.78" />
     </motion.svg>
 }
